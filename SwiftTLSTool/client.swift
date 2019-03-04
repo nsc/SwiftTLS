845c5fa981cb835e3095d377ad7e7b50b419cd73
Nico Schmidt <nico@savoysoftware.com>
2019-03-04 19:27:35 +0100

    Add missing BigIntContext in client.swift (fixes #13)

--- SwiftTLSTool/client.swift
+++ SwiftTLSTool/client.swift
@@ -19,36 +19,38 @@ func connectTo(host : String, port : UInt16 = 443, supportedVersions: [TLSProtoc
     
     // Connect twice to test session resumption
     var context: TLSClientContext? = nil
-    var client: TLSClient
-    for _ in 0..<2 {
-        do {
-            print("Connecting to \(host):\(port)")
-            client = TLSClient(configuration: configuration, context: context)
-            
-            let requestData = [UInt8]("GET / HTTP/1.1\r\nHost: \(host)\r\nUser-Agent: SwiftTLS\r\nConnection: Close\r\n\r\n".utf8)
-            let earlyDataWasSent = try client.connect(hostname: host, port: port, withEarlyData: Data(bytes: requestData))
-            
-            if context == nil {
-                context = client.context as? TLSClientContext
+    BigInt.withContext { _ in
+        var client: TLSClient
+        for _ in 0..<2 {
+            do {
+                print("Connecting to \(host):\(port)")
+                client = TLSClient(configuration: configuration, context: context)
+                
+                let requestData = [UInt8]("GET / HTTP/1.1\r\nHost: \(host)\r\nUser-Agent: SwiftTLS\r\nConnection: Close\r\n\r\n".utf8)
+                let earlyDataWasSent = try client.connect(hostname: host, port: port, withEarlyData: Data(bytes: requestData))
+                
+                if context == nil {
+                    context = client.context as? TLSClientContext
+                }
+                
+                print("Connection established using cipher suite \(client.cipherSuite!)")
+                
+                if !earlyDataWasSent {
+                    try client.write(requestData)
+                }
+                
+                let data = try client.read(count: 4096)
+                if data.count == 0 {
+                    break
+                }
+                print("\(data.count) bytes read.")
+                print("\(String.fromUTF8Bytes(data)!)")
             }
-            
-            print("Connection established using cipher suite \(client.cipherSuite!)")
-            
-            if !earlyDataWasSent {
-                try client.write(requestData)
+            catch (let error) {
+                client.close()
+                
+                print("Error: \(error)")
             }
-            
-            let data = try client.read(count: 4096)
-            if data.count == 0 {
-                break
-            }
-            print("\(data.count) bytes read.")
-            print("\(String.fromUTF8Bytes(data)!)")
-        }
-        catch (let error) {
-            client.close()
-            
-            print("Error: \(error)")
         }
     }
 }
